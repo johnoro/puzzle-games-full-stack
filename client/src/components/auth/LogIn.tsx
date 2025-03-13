825580bdda3a7c71c4b26ae50469d57cec1186de
johnoro <officialjohnorourke@gmail.com>
2025-03-12 22:35:34 -0400

    fixes more text colors

--- client/src/components/auth/LogIn.tsx
+++ client/src/components/auth/LogIn.tsx
@@ -53,7 +53,7 @@ const LogIn: React.FC = () => {
 						type='email'
 						value={email}
 						onChange={e => setEmail(e.target.value)}
-						className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+						className='text-black w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
 						disabled={state.loading}
 					/>
 				</div>
--- client/src/components/auth/SignUp.tsx
+++ client/src/components/auth/SignUp.tsx
@@ -65,7 +65,7 @@ const SignUp: React.FC = () => {
 						type='text'
 						value={username}
 						onChange={e => setUsername(e.target.value)}
-						className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+						className='text-black w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
 						disabled={state.loading}
 					/>
 				</div>
